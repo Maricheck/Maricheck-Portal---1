cc89cd446ac517c5320a365efb113605edf5103b
Maricheck <faizushaikh0220@gmail.com>
2025-08-09 16:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,3 +63,5 @@ with app.app_context():
 @login_manager.user_loader
 def load_user(user_id):
     return models.Admin.query.get(int(user_id))
+    if __name__ == "__main__":
+    app.run(debug=True)
